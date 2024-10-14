9e7956a73429745c1b22b6ba72d1006813b943aa
Kaan Kafali <99081499@mydavinci.nl>
2024-10-14 11:28:27 +0200

    Update afronden-naar-stuivers.py

--- module 1/Deel 4.5/afronden-naar-stuivers.py
+++ module 1/Deel 4.5/afronden-naar-stuivers.py
@@ -1,7 +1,33 @@
+from test_lib import test, report
+
 def snapint(int): return round(int * 20) / 20
 
-print(snapint(122.00))
-print(snapint(2398.95))
-print(snapint(224.23))
 print(snapint(1301.02))
 print(snapint(305.99))
+
+input = snapint(122.00)
+expect_content = 122.0
+name = f'input: {input} output: {snapint(input)}'
+test(name, expect_content, snapint(input))
+
+input = snapint(2398.95)
+expect_content = 2398.95
+name = f'input: {input} output: {snapint(input)}'
+test(name, expect_content, snapint(input))
+
+input = snapint(224.23)
+expect_content = 224.25
+name = f'input: {input} output: {snapint(input)}'
+test(name, expect_content, snapint(input))
+
+input = snapint(1301.02)
+expect_content = 1301.0
+name = f'input: {input} output: {snapint(input)}'
+test(name, expect_content, snapint(input))
+
+input = snapint(305.99)
+expect_content = 306.0
+name = f'input: {input} output: {snapint(input)}'
+test(name, expect_content, snapint(input))
+
+if __name__ == "__main__": report()
