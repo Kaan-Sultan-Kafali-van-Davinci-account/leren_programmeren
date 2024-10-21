84db37204cf106fb3ed931f9af421142efd68e85
Kaan Kafali <99081499@mydavinci.nl>
2024-10-21 09:43:49 +0200

    verander dan

--- module 2/Deel 2/deck van speelkaarten.py
+++ module 2/Deel 2/deck van speelkaarten.py
@@ -1,11 +1,9 @@
 import random
 
-soorten = ("Harten", "Klaveren", "Scheppen", "Ruiten")
-waarden = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Boer", "Vrouw", "Heer", "Aas")
-
+soorten = ("Harten", "Klaveren", "Scheppen", "Ruiten"); waarden = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Boer", "Vrouw", "Heer", "Aas")
 deck = [f"{waarde} van {soort}" for soort in soorten for waarde in waarden] + ["Joker 1", "Joker 2"]
 
 random.shuffle(deck)
 print("De bovenste 7 kaarten zijn: ")
 for x in range(7): print(deck.pop(0))
-print(f"Overgebleven deck ({len(deck)}):", deck)
+print(f"Overgebleven deck (aantal {len(deck)}):", deck)
